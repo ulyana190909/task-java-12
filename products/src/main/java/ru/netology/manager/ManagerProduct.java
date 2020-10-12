3298c0c59a398a0eba8a06566e618eeb4cef52d1
Uliana Utina <ulyana190909@yandex.ru>
2020-10-12 14:55:34 +0300

    черновик второго задания

--- products/src/main/java/ru/netology/manager/ManagerProduct.java
+++ products/src/main/java/ru/netology/manager/ManagerProduct.java
@@ -17,33 +17,10 @@ public class ManagerProduct {
         repository.save(item);
     }
 
-    public boolean matches(Product product, String search) {
-        if (product instanceof Book) {
-            Book book = (Book) product;
-            if (book.getName().equalsIgnoreCase(search)) {
-                return true;
-            }
-            if (book.getAuthor().equalsIgnoreCase(search)) {
-                return true;
-            }
-            return false;
-        }
-        if (product instanceof SmartPhone) {
-            SmartPhone smartPhone = (SmartPhone) product;
-            if (smartPhone.getName().equalsIgnoreCase(search)) {
-                return true;
-            }
-            if (smartPhone.getManufacturer().equalsIgnoreCase(search)) {
-                return true;
-            }
-            return false;
-        }
-        return false;
-    }
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];
-        for (Product product : repository.findAll())) {
-            if (ManagerProduct.matches(product, text) {
+        for (Product product : repository.findAll()) {
+            if (product.matches(text)) {
                 Product[] tmp = new Product[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
                 tmp[tmp.length - 1] = product;
